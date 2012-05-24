fb5d582fb917a8999cab9745dc4ba5f4a46a857e
Michele Salvini <info@slv.dj>
2012-05-24 15:27:51 +0200

    May 24, 2012

--- index.php
+++ index.php
@@ -1,30 +1,32 @@
 <?php
-/*
-* Author: Michele Salvini
-* info@slv.dj
+/**
+* @author: Michele Salvini
+* @mail: info@slv.dj
 *
 * GET Call to index.php with parameter 'id' corresponding to URL to scrape
 *
-* ex: http://www.example.com/index.php?id=www.domaintoscrape.com
+* request example: http://www.example.com/index.php?id=www.domaintoscrape.com
 *
 */
 
-/* Comment the next 2 lines if you don't require to be logged in */
-session_start();
-if (!isset($_SESSION['user_id'])) die('Not logged in');
-/* End session check  */
 
-
-function httpGet($Url, $headers = array()){
-	$ch=curl_init();
+function httpGet ($Url, $headers = array())
+{
+	$ch = curl_init();
 	curl_setopt($ch, CURLOPT_URL, $Url);
 	curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 	curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
 	$result = curl_exec($ch);
 	$info = curl_getinfo($ch);
+
 	curl_close($ch);
-	return array('result' => $result, 'info' => $info);
+
+	return array(
+		'result' => $result,
+		'info' => $info
+	);
 }
 
 $curl = httpGet('http://' . str_replace('http://' , '', $_GET['id']));
@@ -50,9 +52,8 @@ if(!preg_match('#\<meta.*property="og:title.*content="(?P<value>.+)".*\>#i', $co
 if(!preg_match('#\<meta.*property="og:description.*content="(?P<value>.+)".*\>#i', $content, $description))
  	if(!preg_match('#\<meta.*content="(?P<value>.+)".*property="og:description.*\>#i', $content, $description))
 	 	if(!preg_match('#\<meta.*content="(?P<value>.+)".*name="description.*\>#i', $content, $description))
-		 	if(!preg_match('#\<meta.*name="description.*content="(?P<value>.+)".*\>#i', $content, $description)) {
+		 	if(!preg_match('#\<meta.*name="description.*content="(?P<value>.+)".*\>#i', $content, $description))
 				$description['value'] = '';
-		 	}
 
 if(!preg_match('#\<meta.*property="og:url.*content="(?P<value>.+)".*\>#i', $content, $url))
 	if(!preg_match('#\<meta.*content="(?P<value>.+)".*property="og:url.*\>#i', $content, $url))
@@ -67,7 +68,7 @@ foreach ($scraped_img['value'] as $i) {
 	if (preg_match("#^\/.*#", $i)) $img['value'][] = $page_root['value'] . $i;
 	else if (preg_match("#^https?:\/\/.*#", $i)) $img['value'][] = $i;
 	else $img['value'][] = $page_path['value'] . '/' . $i;
-} 
+}
 
 $out = array(
 	'description' => (string)trim($description['value']),
